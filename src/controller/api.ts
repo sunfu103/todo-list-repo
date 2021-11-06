a53112d227e8d3dfd2bb08c05c4018d52a46e721
sunfu103 <sunfu103@163.com>
2021-11-06 13:23:30 +0800

    logic

--- src/controller/api.ts
+++ src/controller/api.ts
@@ -32,35 +32,35 @@ export class APIController {
   @Inject()
   todoItemService: TodoItemService;
 
-  @Post('/get_user')
+  @Post('user/get_user')
   async getUser(@Query() uid: string): Promise<IGetUserResponse> {
     const user = await this.userService.getUser({ uid });
     return { success: true, message: 'OK', data: user };
   }
 
-  @Get('/get')
+  @Get('todo/get')
   async get(@Query() id: number): Promise<IGetTodoItemResponse> {
     const todoItems = await this.todoItemService.get(id);
     return { success: true, message: 'OK', data: todoItems };
   }
 
-  @Post('/save')
+  @Post('todo/save')
   async save(@Body(ALL) todoItem: TNgaTodoItem): Promise<IGetTodoItemResponse> {
     const result = await this.todoItemService.save(todoItem);
     return { success: true, message: 'OK', data: result };
   }
 
-  @Put('/put')
+  @Put('todo/put')
   async put(@Body(ALL) todoItem: TNgaTodoItem): Promise<IGetTodoItemResponse> {
     const result = await this.todoItemService.put(todoItem);
     return { success: true, message: 'OK', data: result };
   }
-  @Get('/list')
+  @Get('todo/list')
   async list(): Promise<IGetTodoItemResponse> {
     const todoItems = await this.todoItemService.list();
     return { success: true, message: 'OK', data: todoItems };
   }
-  @Del('/delete')
+  @Del('todo/delete')
   async delete(@Query() id: number): Promise<IGetTodoItemResponse> {
     const todoItems = await this.todoItemService.delete(id);
     return { success: true, message: 'OK', data: todoItems };
