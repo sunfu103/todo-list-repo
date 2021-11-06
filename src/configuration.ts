343eb218e98ee4636350892867e9c87e818a7a94
yujiayue <jiayue10@staff.weibo.com>
2021-11-06 14:23:53 +0800

    add swagger

--- src/configuration.ts
+++ src/configuration.ts
@@ -2,7 +2,16 @@ import { App, Configuration } from '@midwayjs/decorator';
 import { ILifeCycle } from '@midwayjs/core';
 import { Application } from 'egg';
 import { join } from 'path';
+import * as swagger from '@midwayjs/swagger';
 
+@Configuration({
+  imports: [
+    swagger
+  ]
+})
+export class ContainerConfiguration {
+
+}
 @Configuration({
   importConfigs: [join(__dirname, './config')],
   conflictCheck: true,
